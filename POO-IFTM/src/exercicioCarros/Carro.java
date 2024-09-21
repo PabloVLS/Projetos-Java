99c99f3101ed64fc9a9102526a76b019f645fadf
Pablo Vinicius <128602954+PabloVLS@users.noreply.github.com>
2024-09-20 21:52:32 -0300

    Update Carro.java

--- POO-IFTM/src/exercicioCarros/Carro.java
+++ POO-IFTM/src/exercicioCarros/Carro.java
@@ -1,13 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package exercicioCarros;
 
 /**
  *
- * @author IFTM
+ * @author Pablo
  */
 public class Carro {
+    public String marca;
+    public String modelo;
+    public int ano;
+    public double valor;
     
+    public void mostrarInformacoes(){
+        System.out.println("Marca:"+marca);
+        System.out.println("Modelo:"+modelo);
+        System.out.println("Ano:"+ano);
+    }
+    
+    public void verificarTabelaFipe(){
+        System.out.println("Digite o valor do carro:");
+        
+    }
 }
