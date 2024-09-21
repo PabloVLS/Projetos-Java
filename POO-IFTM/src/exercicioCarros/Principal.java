5fb0500f12f59d1e25d29b9dc345a130a8cbfd5f
Pablo Vinicius <128602954+PabloVLS@users.noreply.github.com>
2024-09-21 00:07:10 -0300

    Update Principal.java

--- POO-IFTM/src/exercicioCarros/Principal.java
+++ POO-IFTM/src/exercicioCarros/Principal.java
@@ -1,12 +1,30 @@
 
 package exercicioCarros;
-
+import java.util.Scanner;
 /**
  *
  * @author Pablo
  */
-public class Principal {
-    public static void main(String[] args) {
+public class Carro {
+    Scanner scan = new Scanner(System.in);
+    public String marca;
+    public String modelo;
+    public int ano;
+    public double valor;
+    public double valorFipe=valor;
     
+    public void mostrarInformacoes(){
+        System.out.println("\n\nMarca:"+marca);
+        System.out.println("Modelo:"+modelo);
+        System.out.println("Ano:"+ano);
+        System.out.println("Valor:"+valorFipe);
+    }
+    
+    public void verificarTabelaFipe(){
+        System.out.println("\nDigite o valor do carro(FIPE):");
+        valor=scan.nextDouble();
+        
+        valorFipe=valor*1.20;
+        System.out.println("O valor do carro com acrescimo de 20% será de:"+valorFipe);
     }
 }
