e66c2ee15627e0e4e9fd0e554fa417be8f384586
Pablo Vinicius <128602954+PabloVLS@users.noreply.github.com>
2024-10-04 22:23:47 -0300

    Update Principal.java

--- AulaPOO4-GetterAndSetter/src/aulagetterandsetter/Principal.java
+++ AulaPOO4-GetterAndSetter/src/aulagetterandsetter/Principal.java
@@ -1,19 +1,29 @@
+package exercicioCarros;
 
-package aulagetterandsetter;
+import java.util.Scanner;
 
 /**
  *
  * @author Pablo
  */
 public class Principal {
-
-    
     public static void main(String[] args) {
-        Carro fusca = new Carro();
-        fusca.setModelo("Fusca");
-        fusca.setMarca("Volkswagen");
-        fusca.setAno(1973);
-        System.out.println("Carro: "+fusca.getMarca()+" - " +fusca.getModelo() + "\nAno: "+fusca.getAno());
+        
+        
+        Carro carro1 = new Carro("Chevrolet", "Onix", 2011, 52000);
+        carro1.mostrarInformacoes();
+        carro1.verificarTabelaFipe(52000);
+        Carro carro2 = new Carro("Ford", "Ford-Gt", 2020, 1230000);
+        carro2.mostrarInformacoes();
+        carro2.verificarTabelaFipe(1230000);
+        Carro carro3 = new Carro("Ferrari", "599XX", 2014, 7085000);
+        carro3.mostrarInformacoes();
+        carro2.verificarTabelaFipe(7085000);
+       
+        
+         
+        
+        
+        
     }
-    
 }
