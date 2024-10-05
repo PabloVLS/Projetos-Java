9228af382ae6ef1df07425282aa71b7a141a3bc5
Pablo Vinicius <128602954+PabloVLS@users.noreply.github.com>
2024-10-04 22:23:35 -0300

    Update Carro.java

--- AulaPOO4-GetterAndSetter/src/aulagetterandsetter/Carro.java
+++ AulaPOO4-GetterAndSetter/src/aulagetterandsetter/Carro.java
@@ -1,17 +1,43 @@
 
-package aulagetterandsetter;
-
+package exercicioCarros;
+import java.util.Scanner;
 /**
  *
  * @author Pablo
  */
 public class Carro {
+    Scanner scan = new Scanner(System.in);
     private String marca;
     private String modelo;
     private int ano;
+    private double valor;
+    private double valorFipe=valor;
     
-    //metodos getters recuperam informação
-    // metodos setters adicionam informação
+    public Carro(){
+    
+    }
+    
+    public Carro(String marca, String modelo, int ano, double valor) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.valor = valor;
+    }
+    
+    public void mostrarInformacoes(){
+        System.out.println("\n\nMarca:"+marca);
+        System.out.println("Modelo:"+modelo);
+        System.out.println("Ano:"+ano);
+        
+    }
+    
+    public void verificarTabelaFipe(double precoCarro){
+        
+        double valorFipe = precoCarro * 1.20; // Calcular e armazenar o valorFipe localmente
+        System.out.println("O valor do " + this.modelo + " com acréscimo de 20% será de: " + valorFipe);
+    }
+
+    
 
     public String getMarca() {
         return marca;
@@ -36,6 +62,22 @@ public class Carro {
     public void setAno(int ano) {
         this.ano = ano;
     }
-    
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValorFipe() {
+        return valorFipe;
+    }
+
+    public void setValorFipe(double valorFipe) {
+        this.valorFipe = valorFipe;
+    }
+
     
 }
