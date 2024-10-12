4fb828ad24b566f31e27067f2467b07e6dea818c
Pablo Vinicius <128602954+PabloVLS@users.noreply.github.com>
2024-10-12 00:29:55 -0300

    Add files via upload

---
+++ Consullta/src/consullta/Consulta.java
@@ -0,0 +1,84 @@
+package consullta;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Consulta {
+    private  String nomeDoPaciente = "";
+    private  String dataDeNascimento = "";
+    private  String profissao= "";
+    private  boolean convenio = false;
+
+    public Consulta() {
+     
+    }
+
+    public Consulta(String nomeDoPaciente, String dataDeNascimento, String profissao, boolean convenio) {
+        this.nomeDoPaciente = nomeDoPaciente;
+        this.dataDeNascimento = dataDeNascimento;
+        this.profissao = profissao;
+        this.convenio = convenio;
+    }
+    
+    public Consulta(String nomeDoPaciente, String dataDeNascimento) {
+        
+        this.nomeDoPaciente = nomeDoPaciente;
+        this.dataDeNascimento = dataDeNascimento;
+    }
+    
+    public String getNomeDoPaciente() {
+        return nomeDoPaciente;
+    }
+
+    public void setNomeDoPaciente(String nomeDoPaciente) {
+        if (nomeDoPaciente == null || nomeDoPaciente.equals("")) {
+            this.nomeDoPaciente = "Sem preenchimento";
+        } else {
+            this.nomeDoPaciente = nomeDoPaciente;
+        }
+    }
+
+    public String getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(String dataDeNascimento) {
+        if (dataDeNascimento == null || dataDeNascimento.equals("")) {
+            this.dataDeNascimento = "Sem preenchimento";
+        } else {
+            this.dataDeNascimento = dataDeNascimento;
+        }
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        if (profissao == null || profissao.equals("")) {
+            this.profissao = "Sem preenchimento";
+        } else {
+            this.profissao = profissao;
+        }
+    }
+   public String getConvenio() {
+       if(this.convenio == true){
+            return "Sim";
+       }else{
+           return "Não";
+       }
+    }
+    
+    public void setConvenio(boolean convenio) {
+        this.convenio = convenio;
+    }
+    
+    public void imprimir(){
+        System.out.println("\nNome: "+getNomeDoPaciente());
+        System.out.println("Data de Nascimento: "+getDataDeNascimento());
+        System.out.println("Profissão: "+getProfissao());
+        System.out.println("Convenio: "+getConvenio());
+    }
+    
+}
---
+++ Consullta/src/consullta/Principal.java
@@ -0,0 +1,65 @@
+package consullta;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        // Usado construtor
+        Consulta consulta1 = new Consulta("Roberto Macedo Pires", "18/09/1987");
+        consulta1.setProfissao(null);
+        consulta1.setConvenio(false);
+        consulta1.imprimir();
+        
+        Consulta consulta2 = new Consulta("\nReinaldo Antônio do Bairro das Industrias", "04/03/1972");
+        consulta2.setProfissao("Membro da Torcida Organizada Galoucura");
+        consulta2.setConvenio(true);
+        consulta2.imprimir();
+        
+        Consulta consulta3 = new Consulta("\nRonaldinho Gaucho", "08/06/1986");
+        consulta3.setProfissao("Ex-Jogador de Futebol");
+        consulta3.setConvenio(false);
+        consulta3.imprimir();
+        
+        
+        
+        
+        // Usado Getter and Setter
+         Consulta consulta4 = new Consulta();
+         consulta4.setNomeDoPaciente("");
+         consulta4.setDataDeNascimento("");
+         consulta4.setProfissao("");
+         consulta4.setConvenio(false);
+         consulta4.imprimir();
+         
+         
+         Consulta consulta5 = new Consulta();
+         consulta5.setNomeDoPaciente("\nJair Messias Bolsonaro");
+         consulta5.setDataDeNascimento("04/04/1968");
+         consulta5.setProfissao("Ex-Presidente");
+         consulta5.setConvenio(true);
+         consulta5.imprimir();
+         
+         Consulta consulta6 = new Consulta();
+         consulta6.setNomeDoPaciente("Luis Inacio Lula da Silva");
+         consulta6.setDataDeNascimento("08/10/1948");
+         consulta6.setProfissao("Presidente");
+         consulta6.setConvenio(false);
+         consulta6.imprimir();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
