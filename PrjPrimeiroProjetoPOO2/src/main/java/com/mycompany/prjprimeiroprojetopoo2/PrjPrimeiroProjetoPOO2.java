819a5e431ce4c466790fbfedc8cab94ba1f85f4b
Pablo Vinicius <128602954+PabloVLS@users.noreply.github.com>
2025-03-10 22:15:36 -0300

    Jframe

---
+++ PrjPrimeiroProjetoPOO2/src/main/java/com/mycompany/prjprimeiroprojetopoo2/PrjPrimeiroProjetoPOO2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prjprimeiroprojetopoo2;
+
+/**
+ *
+ * @author IFTM
+ */
+public class PrjPrimeiroProjetoPOO2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
