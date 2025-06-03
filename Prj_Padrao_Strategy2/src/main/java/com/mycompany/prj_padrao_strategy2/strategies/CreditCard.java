2c14074ad74630b9aef0999cfdcb55ec40b96764
Pablo Vinicius <128602954+PabloVLS@users.noreply.github.com>
2025-06-02 22:19:59 -0300

    Add files via upload

--- Prj_Padrao_Strategy2/src/main/java/com/mycompany/prj_padrao_strategy2/strategies/CreditCard.java
+++ Prj_Padrao_Strategy2/src/main/java/com/mycompany/prj_padrao_strategy2/strategies/CreditCard.java
@@ -5,5 +5,23 @@ package com.mycompany.prj_padrao_strategy2.strategies;
  * @author IFTM
  */
 public class CreditCard {
+    private int amount;
+    private String number;
+    private String date;
+    private String cvv;
     
+    CreditCard(String number, String date, String cvv){
+        this.amount = 100_000;
+        this.number = number;
+        this.date = date;
+        this.cvv = cvv;
+    }
+    
+    public void setAmount(int amount){
+        this.amount = amount;
+    }
+    
+    public int getAmount(){
+        return amount;
+    }
 }
--- Prj_Padrao_Strategy2/src/main/java/com/mycompany/prj_padrao_strategy2/strategies/Order.java
+++ Prj_Padrao_Strategy2/src/main/java/com/mycompany/prj_padrao_strategy2/strategies/Order.java
@@ -5,5 +5,26 @@ package com.mycompany.prj_padrao_strategy2.strategies;
  * @author IFTM
  */
 public class Order {
+    private int totalCost =0;
+    private boolean isClosed = false;
     
+    public void processOrder(PayStrategy strategy){
+        strategy.collectPaymentDetails();
+    }
+    
+    public void setTotalCost(int cost){
+        this.totalCost += cost;
+    }
+    
+    public int getTotalCost(){
+        return totalCost;
+    }
+    
+    public boolean isClosed(){
+        return isClosed;
+    }
+    
+    public void setClosed(){
+        isClosed = true;
+    }
 }
